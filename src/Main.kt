e1aa188505492552c48ac1f43cbadd56d6e232b0
mul1junior <munyithya.muli@strathmore.edu>
2026-08-30 10:02:32 +0300

    Commit 2: Data Types

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 // Concept Note:
-// Group 3-[Michelle Muiruri-174904,Angel Muthoni-189733,Fareed Kamau-193339,Nicole Indimuli-191322,Muli Munyithya-184070,Tichina Naitore-188142]
+// Group 3-[Michelle Muiruri-174904,
+//          Angel Muthoni-189733,
+//          Fareed Kamau-193339,
+//          Nicole Indimuli-191322,
+//          Muli Junior-184070,
+//          Tichina Naitore-188142]
 
 fun main() {
     val appName = "Community Infrastructure Reporting Management"
@@ -19,7 +24,14 @@ fun main() {
     val monthsSinceReported = 2
 
     status = "In progess"
-    //--something to be here guyyss!!!
+
+// : DATA TYPES(Muli)
+    val reporterName: String = "Njeri Michelle"
+    val roadCondition: String = "Damaged"
+    val numberOfPhotos: Int = 3
+    val estimatedRepairCost: Double = 15000.50
+    val isEmergency: Boolean = true
+    val priorityCode: Char = 'H'
 
     println("=".repeat(40))
     println(appName)
@@ -31,12 +43,19 @@ fun main() {
     println("Current Status: $status")
     println("SeverityCode: $severityCode")
     println("ReportedByAdmin: $reportedByAdmin")
+    println("Reported By: $reporterName")
+    println("Road Condition: $roadCondition")
+    println("Number of Photos: $numberOfPhotos")
+    println("Estimated Repair Cost: KES $estimatedRepairCost")
+    println("Emergency Report: $isEmergency")
+    println("Priority Code: $priorityCode")
     println()
 
     println("Total reports logged today: ${totalReportsToday}")
     println("AverageResponse time: ${averageResponseHours} hours")
     println("Months since first reported: ${monthsSinceReported}")
     println("=".repeat(40))
+
 
 
 
